858960b0a14d8286410a8be462925820673cc928
MyamotoShi <125669494+MusashiShI@users.noreply.github.com>
2024-04-17 16:51:48 +0100

    Atualização de Codigo
    Adição de Menu: Administração e Clientes
    Aumento da Organização

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 #include "Lista.h"
 #include "Hashing.h"
@@ -27,8 +28,10 @@ void Exemplo_Hashing()
 int Menu()
 {
     printf(" --------------------------- \n");
-    printf("| 1- Biblioteca             |\n");
-    printf("| 2- Livros                 |\n");
+    printf("| 1- Administração          |\n");
+    printf("| 2- Cliente                |\n");
+    printf("| 3- Biblioteca             |\n");
+    printf("| 4- Livros                 |\n");
     printf("| 0-Sair                    |\n");
     printf(" ---------------------------\n");
     int op;
@@ -37,15 +40,43 @@ int Menu()
 }
 
 
+int MenuAdm ()
+{
+    printf(" ---------------------------\n");
+    printf("|       ADMINISTRAÇÃO       |\n");
+    printf(" ---------------------------\n");
+
+    int opadm;
+    opadm = LerInteiro("Qual a o opcao? ");
+    return opadm;
+}
+
+int Menuclient ()
+{
+    printf(" ---------------------------\n");
+    printf("|          CLIENTE          |\n"); // Na requesição do livro o utilizador tem de adicionar as suas informaçõs tipo nome e ssas cenas.
+    printf(" ---------------------------\n");
+
+    int opclient;
+    opclient = LerInteiro("Qual a o opcao? ");
+    return opclient;
+}
+
+
+
 int MenuBiblio()
 {
-    printf(" ------------------------------- \n");
-    printf("| 1- Adicionar Livro            |\n");
-    printf("| 2- Remover Livro              |\n");
-    printf("| 3- Listar Livros              |\n");
-    printf("| 4- Determinar Area Superior   |\n");
-    printf("| 0-Sair                        |\n");
-    printf(" -------------------------------\n");
+    printf(" ---------------------------------------\n");
+    printf("| 1- Adicionar livro                    |\n");
+    printf("| 2- Remover livro                      |\n");
+    printf("| 3- Listar livros                      |\n");
+    printf("| 4- Determinar area com mais livros    |\n");
+    printf("| 5- Verificar existencia de um livro   |\n");
+    printf("| 6- Livro mais requesitado             |\n");
+    printf("| 7- Livro mais recente                 |\n");
+    printf("| 8- Area mais requesitada              |\n");
+    printf("| 0-Sair                                |\n");
+    printf(" ---------------------------------------\n");
     int opBiblio;
     opBiblio = LerInteiro("Qual a o opcao? ");
     return opBiblio;
@@ -54,8 +85,9 @@ int MenuBiblio()
 int MenuLivro ()
 {
     printf(" --------------------------- \n");
-    printf("| 1- Biblioteca             |\n");
-    printf("| 2- Livros                 |\n");
+    printf("| 1- Requesitar Livro       |\n"); // Na requesição do livro o utilizador tem de adicionar as suas informaçõs tipo nome e ssas cenas.
+    printf("| 2- Devolver Livro         |\n");
+    printf("| 5- Verificar Existenc     |\n");
     printf("| 0-Sair                    |\n");
     printf(" ---------------------------\n");
     int opLivro;
@@ -72,25 +104,67 @@ int MenuLivro ()
 
 
 
-
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
     printf("Projecto-Biblioteca-Versao-Base!\n");
     //Exemplo_Hashing();
     BIBLIOTECA *Bib;
     Bib = CriarBiblioteca("Biblioteca-ESTGV", "log.txt");
-    int OP, OPB, OPL;
+    int OP, OPB, OPL, OPA, OPC;
     do
     {
         system("cls");
         OP = Menu();
         switch(OP)
         {
+// -------------------- SEPARAÇÃO MENUS ----------------------
+// --------------------- ADMINISTRAÇÃO -----------------------
             case 1:
                 system("cls");
-                MenuBiblio;
+                MenuAdm;
                 do
                 {
+                    OPA = MenuAdm();
+                    switch(OPA)
+                    {
+                       case 1:
+                       break;
+                    }
+                }while (OPA != 0);
+            break;
+
+
+
+// -------------------- SEPARAÇÃO MENUS ----------------------
+// ------------------------ CLIENTE --------------------------
+
+
+            case 2:  system("cls");
+                     Menuclient;
+                                     do
+                {
+                    OPC = Menuclient();
+                    switch(OPC)
+                    {
+                       case 1:
+                        break;
+
+
+                    }
+                }while (OPC != 0);
+            break;
+
+
+// -------------------- SEPARAÇÃO MENUS ----------------------
+// ---------------------- BIBLIOTECA -------------------------
+
+
+
+            case 3:  system("cls");
+                     MenuBiblio;
+                do
+                 {
                     OPB = MenuBiblio();
                     switch(OPB)
                     {
@@ -103,31 +177,31 @@ int main()
             break;
 
 
-
-
-
-
-
-
 // -------------------- SEPARAÇÃO MENUS ----------------------
+// ------------------------ LIVROS --------------------------
 
 
 
-
-
-
-
-
-
-
-            case 2:  system("cls");
+            case 4:  system("cls");
                     MenuLivro;
-                break;
+                    do
+                 {
+                    OPL = MenuLivro();
+                    switch(OPL)
+                    {
+                       case 1:
+                        break;
+
+
+                    }
+                }while (OPL != 0);
+            break;
 
 
             default:
                 printf("Opcao nao implementada\n"); break;
         }while (OPL != 0);
+
 
     }while (OP != 0);
     DestruirBiblioteca(Bib);
